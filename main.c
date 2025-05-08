0f23c99149120ebfad35dd631556b2e8fb81a277
Jennyfer753 <jennyfer.mbele@edu.ece.fr>
2025-05-08 03:40:40 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,38 +3,44 @@
 #include "allegro_init.h"
 #include "menu.h"
 #include "parties.h"
+#include "gestion_sons.h"
 
 int main() {
-
     BITMAP* buffer = NULL;
-    int x1=300;
-    int x2=500;
-    int y1=250;
-    int y2=350;
     int etat_jeu=0;
+    int etat_son=1;
 
     initialisation_allegro(&buffer);
+    init_son(&etat_son);
 
     while (!key[KEY_ESC]) {
         clear_bitmap(buffer);
 
-        /*if (key[KEY_ENTER] ) {
-            etat_jeu = 1;
-            rest(200);
-        }*///Ne fonctionne pas
-
         switch(etat_jeu) {
             case 0 :
                 ecran_menu(buffer);
-            if ((mouse_b & 1) && (mouse_x >= x1) &&
-                (mouse_x <= x2) && (mouse_y >= y1) &&
-                (mouse_y <= y2)) {
+            bouton_son(buffer, &etat_son);
+
+            if ((mouse_b & 1) && (mouse_x >= 300) &&
+                (mouse_x <= 500) && (mouse_y >= 250) &&
+                (mouse_y <= 290)) {
                 etat_jeu = 1;
                 }
+
+            if ((mouse_b & 1) && (mouse_x >= 300) &&
+               (mouse_x <= 500) && (mouse_y >= 330) &&
+               (mouse_y <= 370)) {
+                etat_jeu = 5;
+               }
+                while (mouse_b & 1) {
+                    if (mouse_b & 1) {
+                    clic(mouse_x, mouse_y, &etat_son);
+                    }
+            }
             break;
 
             case 1:
-                config_partie(buffer);
+                config_partie(buffer,&etat_son);
 
             if (key[KEY_2]) {
                 etat_jeu = 2;
@@ -47,17 +53,17 @@ int main() {
             }
 
             if (mouse_b & 1) {
-                int mx = mouse_x;
-                int my = mouse_y;
-                int rayon = 20;
 
-                if ((mx - 350)*(mx - 350) + (my - 300)*(my - 300) <= rayon*rayon) {
+                  if (mouse_x >= 220 && mouse_x <= 380 &&
+                      mouse_y >= 200 && mouse_y <= 250 ){
                     etat_jeu = 2;
                 }
-                else if ((mx - 400)*(mx - 400) + (my - 300)*(my - 300) <= rayon*rayon) {
+                else if(mouse_x >= 420 && mouse_x <= 580 &&
+                    mouse_y >= 200 && mouse_y <= 250) {
                     etat_jeu = 3;
                 }
-                else if ((mx - 450)*(mx - 450) + (my - 300)*(my - 300) <= rayon*rayon) {
+                else if(mouse_x >= 320 && mouse_x <= 480 &&
+                    mouse_y >= 280 && mouse_y <= 330) {
                     etat_jeu = 4;
                 }
             }
@@ -74,13 +80,16 @@ int main() {
             case 4 :
                 init_1v1v1v1(buffer);
             break;
+
+            case 5:
+                allegro_exit();
+            exit(0);
         }
 
-        blit(buffer, screen,
-            0, 0,
-            0, 0, 800, 600);
-        }
-
+    blit(buffer, screen,
+        0, 0,
+        0, 0, 800, 600);
+}
     destroy_bitmap(buffer);
     return 0;
 }
