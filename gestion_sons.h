5ad825991d10438a6f15ce5f57c2c51bafefeb37
Jennyfer753 <jennyfer.mbele@edu.ece.fr>
2025-05-08 03:44:28 +0200

    Create gestion_sons.h

---
+++ gestion_sons.h
@@ -0,0 +1,8 @@
+#ifndef GESTION_SONS_H
+#define GESTION_SONS_H
+
+void init_son(int* etat_son);
+void bouton_son(BITMAP* buffer, int* etat_son);
+void clic(int x, int y, int* etat_son);
+
+#endif //GESTION_SONS_H
