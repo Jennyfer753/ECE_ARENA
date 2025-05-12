6ebdbe285417ff7487540e58f49798592f70b07b
abigail8678 <rimtetaabigail22@gmail.com>
2025-05-12 20:21:44 +0200

    Update attaque_sort.c

--- attaque_sort.c
+++ attaque_sort.c
@@ -6,15 +6,58 @@
 #include "attaque_sort.h"
 #include <allegro.h>
 
-void attaque_sort_mage(Sort* sort ) {
-
+// Sous-programmes des sorts du Mage
+void sort_pluie_de_glace(Sort* sort){
+  
 }
-void attaque_sort_maitresse(Sort* sort ) {
-
+void sort_sac_de_feu(Sort* sort){
+  
 }
-void attaque_sort_savant(Sort* sort ) {
-
+void sort_tornade(Sort* sort){
+  
 }
-void attaque_sort_archère(Sort* sort ) {
+void sort_cercueil_de_boue(Sort* sort){
+  
+}
 
+// Sous-programmes des sorts de la Maîtresse Dragon
+void sort_boule_de_feu(Sort* sort){
+  
+}
+void sort_multi_boule_de_feu(Sort* sort){
+  
+}
+void sort_bebe_dragon_feu(Sort* sort){
+  
+}
+void sort_susano_dragon(Sort* sort){
+  
+}
+
+// Sous-programmes des sorts du Savant Fou
+void sort_bombe(Sort* sort){
+  
+}
+void sort_canon(Sort* sort){
+  
+}
+void sort_mitraillette(Sort* sort){
+  
+}
+void sort_super_bazooka(Sort* sort){
+  
+}
+
+// Sous-programmes des sorts de l’Archère
+void sort_tir_normal(Sort* sort){
+  
+}
+void sort_tir_multiple(Sort* sort){
+  
+}
+void sort_fleche_poison(Sort* sort){
+  
+}
+void sort_fleche_foudre(Sort* sort){
+  
 }
