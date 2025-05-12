ef99aded4f5552fd639591c6f926346b1bb3833f
abigail8678 <rimtetaabigail22@gmail.com>
2025-05-12 11:10:48 +0200

    Update attaque_sort.c

--- attaque_sort.c
+++ attaque_sort.c
@@ -1,6 +1,20 @@
 //
 // Created by Admin on 20/04/2025.
 //
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "attaque_sort.h"
+#include <allegro.h>
 
+void attaque_sort_mage(Sort* sort ) {
+
+}
+void attaque_sort_maitresse(Sort* sort ) {
+
+}
+void attaque_sort_savant(Sort* sort ) {
+
+}
+void attaque_sort_archère(Sort* sort ) {
+
+}
