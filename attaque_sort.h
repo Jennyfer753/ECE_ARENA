d9e210d12fb3604bc969af87dcacd5fa92e1fdf3
abigail8678 <rimtetaabigail22@gmail.com>
2025-05-12 20:04:27 +0200

    Update attaque_sort.h

--- attaque_sort.h
+++ attaque_sort.h
@@ -5,20 +5,39 @@
 #ifndef ATTAQUE_SORT_H
 #define ATTAQUE_SORT_H
 
-#endif //ATTAQUE_SORT_H
+#include <allegro.h>
 
-typedef struct Sort{
+// Définition de la structure Sort
+typedef struct Sort {
     int pv_retire;
     int cout_pa;
     char nom[30];
     int portee_min;
     int portee_max;
+} Sort;
 
-}Sort;
+// Prototypes des sorts du Mage
+void sort_pluie_de_glace(Sort* sort);
+void sort_sac_de_feu(Sort* sort);
+void sort_tornade(Sort* sort);
+void sort_cercueil_de_boue(Sort* sort);
 
-void attaque_sort_mage(Sort* sort );
-void attaque_sort_maitresse(Sort* sort );
-void attaque_sort_savant(Sort* sort );
-void attaque_sort_archère(Sort* sort );
+// Prototypes des sorts de la Maîtresse Dragon
+void sort_boule_de_feu(Sort* sort);
+void sort_multi_boule_de_feu(Sort* sort);
+void sort_bebe_dragon_feu(Sort* sort);
+void sort_susano_dragon(Sort* sort);
 
+// Prototypes des sorts du Savant Fou
+void sort_bombe(Sort* sort);
+void sort_canon(Sort* sort);
+void sort_mitraillette(Sort* sort);
+void sort_super_bazooka(Sort* sort);
 
+// Prototypes des sorts de l’Archère
+void sort_tir_normal(Sort* sort);
+void sort_tir_multiple(Sort* sort);
+void sort_fleche_poison(Sort* sort);
+void sort_fleche_foudre(Sort* sort);
+
+#endif //ATTAQUE_SORT_H
