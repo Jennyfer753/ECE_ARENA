a34495238a0a1e685209a3bd627ace20e5dd4914
Aloisemrt <aloise.martin@edu.ece.fr>
2025-05-17 00:45:27 +0200

    Create choix_classe.h

---
+++ choix_classe.h
@@ -0,0 +1,7 @@
+
+#ifndef CHOIX_CLASSE_H
+#define CHOIX_CLASSE_H
+#include "allegro.h"
+#include "joueurs.h"
+void choix_perso(BITMAP* buffer, init_joueurs* j,  int id_joueur);
+#endif //CHOIX_CLASSE_H
