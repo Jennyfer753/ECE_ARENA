8e9171d01cfba206a1be74313ea102e99005dd7b
AbigailRMT <rimtetaabigail22@gmail.com>
2025-05-18 23:18:59 +0200

    Update (levraidevrai) main.c

--- (levraidevrai) main.c
+++ (levraidevrai) main.c
@@ -1,7 +1,15 @@
+#include <stdio.h>
+#include <allegro.h>
+#include "allegro_init.h"
+#include "menu.h"
+#include "parties.h"
+#include "gestion_sons.h"
+#include "temps_jeu.h"
+
 #include "gestion_jeu.h"
 
 int main() {
-    lancer_jeu();
+    lancer_jeu(); //L'intégralité du jeu a été codé en sous programmes contenus dans cette procédure
     return 0;
 }
 END_OF_MAIN();
