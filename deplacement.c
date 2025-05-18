780f0745df9d3fec67dea76acbaf8fcf62123088
Aloisemrt <aloise.martin@edu.ece.fr>
2025-05-18 11:55:17 +0200

    Update deplacement.c

--- deplacement.c
+++ deplacement.c
@@ -1,8 +1,9 @@
 #include <allegro.h>
+#include <stdio.h>
 #include "deplacement.h"
 #include "joueurs.h"
 #include "arene.h"
-
+#include "gestion_states.h"
 void zonesDeplacement(BITMAP* buffer, int ligne_joueur, int colonne_joueur, int taille_case, init_joueurs* joueurs, int nb_joueurs) {
 
     int couleur_libre;
@@ -25,9 +26,9 @@ void zonesDeplacement(BITMAP* buffer, int ligne_joueur, int colonne_joueur, int 
     colonne_max = colonne_joueur + 3;
 
     if (ligne_min < 0) ligne_min = 0;
-    if (ligne_max >= SCREEN_H / taille_case) ligne_max = (SCREEN_H / taille_case) - 1;
+    if (ligne_max >= 600/ taille_case) ligne_max = (600/ taille_case) - 1;
     if (colonne_min < 0) colonne_min = 0;
-    if (colonne_max >= (SCREEN_W - 110) / taille_case) colonne_max = ((SCREEN_W - 110) / taille_case) - 1;
+    if (colonne_max >= 690 / taille_case) colonne_max = (690 / taille_case) - 1;
 
     for (int ligne = ligne_min; ligne <= ligne_max; ligne++) {
         for (int colonne = colonne_min; colonne <= colonne_max; colonne++) {
@@ -87,12 +88,13 @@ int calculeDistanceManhattan(int ligne1, int colonne1, int ligne2, int colonne2)
     return diff_ligne + diff_colonne;
 }
 
-void deplacement(BITMAP* buffer, init_joueurs* j, int nb_joueurs) {
+
+int deplacement(BITMAP* buffer, init_joueurs* j, int nb_joueurs, int index_joueur_actuel, int reset) {
     clear_bitmap(buffer);
     int taille_case = 50;
     static int joueur_choisi = -1;
-    int largeur = (SCREEN_W - 110) / taille_case;
-    int hauteur = SCREEN_H / taille_case;
+    int largeur = 690 / taille_case;
+    int hauteur = 600 / taille_case;
     static int position_prec = 0;
     int position_actuel = mouse_b & 1;
 
@@ -101,27 +103,30 @@ void deplacement(BITMAP* buffer, init_joueurs* j, int nb_joueurs) {
     int distance;
     int case_occupee;
 
+    if (reset) {
+        joueur_choisi = -1;
+        return 0;
+    }
+
     if (position_actuel == 1 && position_prec == 0) {
         if (ligne_clic >= 0 && ligne_clic < hauteur && colonne_clic >= 0 && colonne_clic < largeur) {
             if (joueur_choisi == -1) {
-                for (int i = 0; i < nb_joueurs; i++) {
-                    if (j[i].ligne == ligne_clic && j[i].colonne == colonne_clic) {
-                        joueur_choisi = i;
-                    }
+                if (j[index_joueur_actuel].ligne == ligne_clic && j[index_joueur_actuel].colonne == colonne_clic) {
+                    joueur_choisi = index_joueur_actuel;
                 }
-            }
-            else{
-                 distance = calculeDistanceManhattan(j[joueur_choisi].ligne, j[joueur_choisi].colonne, ligne_clic, colonne_clic);
-                 case_occupee = verificationCaseVide(ligne_clic, colonne_clic, j, nb_joueurs, joueur_choisi);
+            } else {
+                distance = calculeDistanceManhattan(j[joueur_choisi].ligne, j[joueur_choisi].colonne, ligne_clic, colonne_clic);
+                case_occupee = verificationCaseVide(ligne_clic, colonne_clic, j, nb_joueurs, joueur_choisi);
 
-                if (case_occupee == 0 && distance <= 3) {
+                if (case_occupee == 0 && distance <= j[joueur_choisi].pm) {
                     j[joueur_choisi].ligne = ligne_clic;
                     j[joueur_choisi].colonne = colonne_clic;
+                    j[joueur_choisi].pm -= distance; // Consommer les PM
                 }
+
                 joueur_choisi = -1;
             }
-        }
-        else{
+        } else {
             joueur_choisi = -1;
         }
     }
@@ -136,9 +141,15 @@ void deplacement(BITMAP* buffer, init_joueurs* j, int nb_joueurs) {
         int x = 110 + j[i].colonne * taille_case + taille_case / 2;
         int y = j[i].ligne * taille_case + taille_case / 2;
         circlefill(buffer, x, y, 20, j[i].couleur);
-        if (i == joueur_choisi) {
+        if (i == index_joueur_actuel) {
             circle(buffer, x, y, 25, makecol(0, 255, 0));
         }
     }
+
     position_prec = position_actuel;
+    state(buffer, j, nb_joueurs, taille_case);
+    return 0;
 }
+
+
+
