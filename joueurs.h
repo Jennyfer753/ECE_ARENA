5f780218c108d39f7c85ae9133c4bc273b112733
AbigailRMT <rimtetaabigail22@gmail.com>
2025-05-18 23:17:39 +0200

    Update joueurs.h

--- joueurs.h
+++ joueurs.h
@@ -1,16 +1,20 @@
+//
+// Created by alois on 18/05/2025.
+//
 
 #ifndef JOUEURS_H
 #define JOUEURS_H
 
+//On définit une structure joueur
 typedef struct {
-    int ligne;
-    int colonne;
-    int couleur;
-    int pv ;
-    int pm;
-    int pa;
-    char classe[4];
-    char nom[51];
+    int ligne; //La Ligne sur laquelle est placée le joueur
+    int colonne; //La colonne sur laquelle il est placé
+    int couleur; //Couleur du joueur
+    int pv ; // Nombre de points de vie
+    int pm; // Nombre de points de mouvement
+    int pa; // Nombre de points d'action
+    char classe[4]; // Classe du personnage : mage, archère, etc.
+    char nom[51]; // Nom du joueur
 } init_joueurs;
 
 #endif //JOUEURS_H
